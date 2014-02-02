7fcee07f9cc9569e7deabae405cbe520bb01f938
Mimi Flynn <mimiflynn@gmail.com>
2014-02-02 18:59:24 -0500

    namespace changed to wpNg

--- app/lib/angular/angular.js
+++ app/lib/angular/angular.js
@@ -4737,8 +4737,8 @@
      * Adding via the $templateCache service:
      *
      * <pre>
-     * var myApp = angular.module('myApp', []);
-     * myApp.run(function($templateCache) {
+     * var wpNg = angular.module('wpNg', []);
+     * wpNg.run(function($templateCache) {
  *   $templateCache.put('templateId.html', 'This is the content of the template');
  * });
      * </pre>
@@ -12581,15 +12581,15 @@
      *
      * **Example**:  Consider the following case. <a name="example"></a>
      *
-     * - your app is hosted at url `http://myapp.example.com/`
+     * - your app is hosted at url `http://wpNg.example.com/`
      * - but some of your templates are hosted on other domains you control such as
      *   `http://srv01.assets.example.com/`,  `http://srv02.assets.example.com/`, etc.
-     * - and you have an open redirect at `http://myapp.example.com/clickThru?...`.
+     * - and you have an open redirect at `http://wpNg.example.com/clickThru?...`.
      *
      * Here is what a secure configuration for this scenario might look like:
      *
      * <pre class="prettyprint">
-     *    angular.module('myApp', []).config(function($sceDelegateProvider) {
+     *    angular.module('wpNg', []).config(function($sceDelegateProvider) {
  *      $sceDelegateProvider.resourceUrlWhitelist([
  *        // Allow same origin resource loads.
  *        'self',
@@ -12598,7 +12598,7 @@
  *
  *      // The blacklist overrides the whitelist so the open redirect here is blocked.
  *      $sceDelegateProvider.resourceUrlBlacklist([
- *        'http://myapp.example.com/clickThru**']);
+ *        'http://wpNg.example.com/clickThru**']);
  *      });
      * </pre>
      */
@@ -13058,7 +13058,7 @@
      * @example
      <example module="mySceApp" deps="angular-sanitize.js">
      <file name="index.html">
-     <div ng-controller="myAppController as myCtrl">
+     <div ng-controller="wpNgController as myCtrl">
      <i ng-bind-html="myCtrl.explicitlyTrustedHtml" id="explicitlyTrustedHtml"></i><br><br>
      <b>User comments</b><br>
      By default, HTML that isn't explicitly trusted (e.g. Alice's comment) is sanitized when
@@ -13077,7 +13077,7 @@
      <file name="script.js">
      var mySceApp = angular.module('mySceApp', ['ngSanitize']);
 
-     mySceApp.controller("myAppController", function myAppController($http, $templateCache, $sce) {
+     mySceApp.controller("wpNgController", function wpNgController($http, $templateCache, $sce) {
     var self = this;
     $http.get("test_data.json", {cache: $templateCache}).success(function(userComments) {
       self.userComments = userComments;
@@ -13127,7 +13127,7 @@
      * That said, here's how you can completely disable SCE:
      *
      * <pre class="prettyprint">
-     *   angular.module('myAppWithSceDisabledmyApp', []).config(function($sceProvider) {
+     *   angular.module('wpNgWithSceDisabledwpNg', []).config(function($sceProvider) {
  *     // Completely disable SCE.  For demonstration purposes only!
  *     // Do not use in new projects.
  *     $sceProvider.enabled(false);
